dc5ead94021d73778cb012e3054fd1e68eaf9774
SUCHUANFU <wl02821547@gmail.com>
2025-10-15 20:41:00 +0800

    use to tailwind css to show  HomePage_28 in vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 // import './index.css'
-import './blog_28.scss'
-import App_28 from './App_28.jsx'
+import './index.css';
+import App_28 from './App_28.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App_28 />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/HomePage_28.jsx
+++ src/pages/HomePage_28.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const HomePage_28 = () => {
   return (
-    <div>
-      <h1>HomePage_28</h1>
-      <h2>chuanfu, 213410128</h2>
+    <div className='flex flex-col justify-center items-center my-8 '>
+      <h1 className='text-2xl text-purple-400'> HomePage_28</h1>
+      <h2 className='text-xl'>chuanfu, 213410128</h2>
     </div>
-  )
-}
-export default HomePage_28
+  );
+};
+export default HomePage_28;
